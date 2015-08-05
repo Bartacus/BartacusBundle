13363f18c6fe110e98327f74fdaabf8b17fed838
Patrik Karisch <p.karisch@pixelart.at>
2015-08-05 20:07:41 +0200

    Bump next dev version

--- src/Kernel/Kernel.php
+++ src/Kernel/Kernel.php
@@ -35,12 +35,12 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
  */
 abstract class Kernel extends BaseKernel
 {
-    const VERSION = '0.2.1';
-    const VERSION_ID = '00201';
+    const VERSION = '0.2.2-DEV';
+    const VERSION_ID = '00202';
     const MAJOR_VERSION = '0';
     const MINOR_VERSION = '2';
-    const RELEASE_VERSION = '1';
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = '2';
+    const EXTRA_VERSION = 'DEV';
 
     /**
      * {@inheritdoc}
