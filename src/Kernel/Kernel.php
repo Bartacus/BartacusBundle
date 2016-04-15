da783b62db3085b37335ae3ef513b96c4f9e13e6
Patrik Karisch <p.karisch@pixelart.at>
2016-04-15 16:41:47 +0200

    Release 0.3.3

--- src/Kernel/Kernel.php
+++ src/Kernel/Kernel.php
@@ -42,12 +42,12 @@ use TYPO3\CMS\Frontend\Utility\EidUtility;
  */
 abstract class Kernel extends BaseKernel
 {
-    const VERSION = '0.3.3-DEV';
+    const VERSION = '0.3.3';
     const VERSION_ID = '00303';
     const MAJOR_VERSION = '0';
     const MINOR_VERSION = '3';
     const RELEASE_VERSION = '3';
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     /**
      * {@inheritdoc}
