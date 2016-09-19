f6f3d0dc8d513328d4d0383ebb2b9303daffdacd
Patrik Karisch <patrik@karisch.guru>
2016-09-19 21:52:07 +0200

    Add a typo3 service bridge for common typo3 stuff

---
+++ Typo3/ServiceBridge.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the BartacusBundle.
+ *
+ * The BartacusBundle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The BartacusBundle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the BartacusBundle. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Bartacus\Bundle\BartacusBundle\Typo3;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\Page\PageRepository;
+
+/**
+ * Service bridge to TYPO3 instantiation and global instances.
+ *
+ * @author Patrik Karisch <p.karisch@pixelart.at>
+ */
+class ServiceBridge
+{
+    /**
+     * Wrapper around {@see GeneralUtility::makeInstance()}.
+     *
+     * @param string $className
+     *
+     * @return object
+     */
+    public function makeInstance(string $className)
+    {
+        return GeneralUtility::makeInstance($className);
+    }
+
+    /**
+     * Get a TYPO3 global into the service container.
+     *
+     * @param string $global
+     *
+     * @return mixed
+     */
+    public function getGlobal(string $global)
+    {
+        return $GLOBALS[$global];
+    }
+
+    /**
+     * @return ContentObjectRenderer
+     */
+    public function getContentObjectRenderer(): ContentObjectRenderer
+    {
+        return $this->getGlobal('TSFE')->cObj;
+    }
+
+    /**
+     * @return PageRepository
+     */
+    public function getPageRepository(): PageRepository
+    {
+        return $this->getGlobal('TSFE')->sys_page;
+    }
+}
