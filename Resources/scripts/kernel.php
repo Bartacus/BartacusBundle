eae69e9301938da81eaee2f0f30d49fec39b09e2
Patrik Karisch <patrik@karisch.guru>
2016-11-30 14:35:22 +0100

    #52 Require the autoloader of the app, not the composer one

--- Resources/scripts/kernel.php
+++ Resources/scripts/kernel.php
@@ -28,7 +28,7 @@ defined('SYMFONY_DEBUG') || define(
 );
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../../../../../vendor/autoload.php';
+$loader = require __DIR__.'/../../../../../app/autoload.php';
 include_once __DIR__.'/../../../../../var/bootstrap.php.cache';
 
 if (SYMFONY_DEBUG) {
