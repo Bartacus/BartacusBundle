a1b8f573ba21dbb1c4558fef3f217842b8644829
Patrik Karisch <patrik@karisch.guru>
2017-08-25 17:03:22 +0200

    Update to Symfony 3.3 with no own autoloader and bootstrap files anymore

--- Resources/scripts/kernel.php
+++ Resources/scripts/kernel.php
@@ -30,8 +30,7 @@ use Symfony\Component\Debug\Debug;
 );
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../../../../../app/autoload.php';
-include_once __DIR__.'/../../../../../var/bootstrap.php.cache';
+$loader = require __DIR__.'/../../../../../vendor/autoload.php';
 
 if (SYMFONY_DEBUG) {
     Debug::enable();
